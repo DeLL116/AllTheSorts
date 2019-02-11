ad9da309008e3af5efa2a30dd8166325be4a2576
Chris Delli Santi <chris.dellisantijr@gmail.com>
2019-02-10 22:11:56 -0500

    Added some comments and documetation

--- app/src/main/java/com/demo/chris/allthesorts/sorts/SortAlgo.kt
+++ app/src/main/java/com/demo/chris/allthesorts/sorts/SortAlgo.kt
@@ -4,6 +4,10 @@ package com.demo.chris.allthesorts.sorts
  * Defines a base type for a Sorting Algorithm class
  */
 interface SortAlgo {
+
+    /** Generic name of the sorting Algorithm */
     val name: String
+
+    /** Backing data to be sorted */
     val data: IntArray
 }
